cd36c767719fb37e0e6dbb843beb4f3b4ebd1cdd
Milena Nobre Magalhaes de Brito <milena.nobre@pampa.compasso>
2022-01-25 15:54:13 -0300

    Desfio + comununicação de componentes

---
+++ vue-cli/intro-components/src/Contadores.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contadores">
+    <h1>Contadores</h1>
+    <app-contador v-for="c in 3" :key="c"/>
+  </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+
+export default {
+    components: {
+      'app-contador': Contador
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
