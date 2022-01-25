697b31f13ae647f869f794016f9f59355ae31c08
Milena Nobre Magalhaes de Brito <milena.nobre@pampa.compasso>
2022-01-25 17:30:13 -0300

    Atualizando anotações, aprendendo assuntos avançados de componentes e iniciando desafio

---
+++ vue-cli/citacoes/src/components/Citacao.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="citacao">
+      <slot name="autor"></slot>
+      <slot></slot>
+      <slot name="fonte"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+      activated() {
+        console.log('activated Citacao')
+      },
+      deactivated() {
+        console.log('deactivated Citacao')
+      }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
