512aad972768ff050465094a2b0b010d2ffe0868
Milena Nobre Magalhaes de Brito <milena.nobre@pampa.compasso>
2022-01-26 14:07:35 -0300

    Desenvolvimento do desafio sobre slot e component

--- vue-cli/comp-avancado-desafio/src/App.vue
+++ vue-cli/comp-avancado-desafio/src/App.vue
@@ -1,14 +1,13 @@
 <template>
 	<div id="app">
 		<span>
-			<button class="vermelho">Carregar Componente Vermelho</button>
-			<button class="verde">Carregar Componente Verde</button>
-			<button class="azul">Carregar Componente Azul</button>
+			<button class="vermelho" @click="componente = 'Vermelho'">Carregar Componente Vermelho</button>
+			<button class="verde" @click="componente = 'Verde'">Carregar Componente Verde</button>
+			<button class="azul" @click="componente = 'Azul'">Carregar Componente Azul</button>
 		</span>
-		
-		<Vermelho />
-		<Verde />
-		<Azul />
+    <component :is="componente">
+      <span>Conteúdo do Componente <strong>{{ componente }}</strong></span>
+    </component>
 	</div>
 </template>
 
@@ -19,7 +18,19 @@ import Azul from './components/Azul.vue'
 
 export default {
 	name: 'app',
-	components: { Vermelho, Verde, Azul },
+	components: {
+    Vermelho,
+    Verde,
+    Azul
+  },
+  data() {
+    return {
+        componente: 'Vermelho',
+    }
+  },
+  methods:{
+
+  }
 }
 </script>
 
