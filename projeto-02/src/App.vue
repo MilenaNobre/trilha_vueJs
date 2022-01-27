44e996f8bb7ea1cf19f6b749ba1380bd0805ba78
Milena Nobre Magalhaes de Brito <milena.nobre@pampa.compasso>
2022-01-27 15:05:44 -0300

    Desenvolvimento do projeto 02

---
+++ projeto-02/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <div class="panel content">
+      <h1> TODO LIST </h1>
+      <p>Projeto #02 </p>
+    </div>
+    <div class="panel">
+      <progress-bar :porcentagem="percent" />
+    </div>
+    <add-button @add="addIssue($event)"/>
+
+    <card-grid :tarefas="tarefas"
+               @tarefasDeleted="deleted"
+               @tarefasChanged="changed">
+
+    </card-grid>
+
+  </div>
+</template>
+
+<script>
+import ProgressBar from "@/components/ProgressBar";
+import AddButton from "@/components/AddButton";
+import CardGrid from "@/components/CardGrid";
+
+
+export default {
+  name: 'App',
+  components: {
+    CardGrid,
+    ProgressBar,
+    AddButton
+  },
+  data(){
+    return {
+      tarefas: []
+    }
+  },
+  watch:{
+
+  },
+  computed:{
+    qtdTrue(){
+      return this.tarefas.filter(el => el.check).length;
+    },
+    percent() {
+      return (this.qtdTrue*100)/ this.tarefas.length | 0;
+    }
+  },
+  methods:{
+    addIssue(issue){
+      const notExist = this.tarefas.filter(el => el.text == issue).length ==0
+      if(notExist) {
+        this.tarefas.push({
+          text: issue,
+          check: false
+        });
+      }
+    },
+    changed(i) {
+      this.tarefas[i].check = !this.tarefas[i].check
+    },
+    deleted(i) {
+      this.tarefas.splice(i, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ projeto-02/src/components/CardGrid.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="task-grid">
+    <template v-if="tarefas.length">
+      <card-tarefa v-for="(tarefa, i) in tarefas" :key="i" :tarefa="tarefa"
+                   @tarefaDeleted="$emit('tarefasDeleted', i)"
+                   @tarefaChanged="$emit('tarefasChanged', i)"></card-tarefa>
+    </template>
+    <p v-else class="no-task">Sua vida está em dia :)</p>
+  </div>
+</template>
+
+<script>
+import CardTarefa from "@/components/CardTarefa";
+export default {
+  name: "CardGrid",
+  components: {CardTarefa},
+  props:{
+    tarefas: { type: Array, required: true }
+  }
+}
+</script>
+
+<style scoped>
+.task-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.task-grid .task {
+  margin: 10px;
+}
+
+.no-task {
+  color: #AAA;
+  font-size: 1.7rem;
+}
+</style>
