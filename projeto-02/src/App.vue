650c1d1340b650bc916a3c390314cb4a6eb04c1c
Milena Nobre Magalhaes de Brito <milena.nobre@pampa.compasso>
2022-01-27 15:30:02 -0300

    Incluindo salvamento das tarefas no localstorage

--- projeto-02/src/App.vue
+++ projeto-02/src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <div class="panel content">
-      <h1> TODO LIST </h1>
-      <p>Projeto #02 </p>
+    <div id="app">
+        <div class="panel content">
+          <h1> TODO LIST </h1>
+          <p>Projeto #02 </p>
+        </div>
+        <div class="panel">
+          <progress-bar :porcentagem="percent" />
+        </div>
+        <add-button @add="addIssue($event)"/>
+
+        <card-grid :tarefas="tarefas"
+                   @tarefasDeleted="deleted"
+                   @tarefasChanged="changed">
+        </card-grid>
     </div>
-    <div class="panel">
-      <progress-bar :porcentagem="percent" />
-    </div>
-    <add-button @add="addIssue($event)"/>
-
-    <card-grid :tarefas="tarefas"
-               @tarefasDeleted="deleted"
-               @tarefasChanged="changed">
-
-    </card-grid>
-
-  </div>
 </template>
 
 <script>
 import ProgressBar from "@/components/ProgressBar";
 import AddButton from "@/components/AddButton";
 import CardGrid from "@/components/CardGrid";
-
 
 export default {
   name: 'App',
@@ -36,15 +33,20 @@ export default {
       tarefas: []
     }
   },
-  watch:{
-
-  },
   computed:{
     qtdTrue(){
       return this.tarefas.filter(el => el.check).length;
     },
     percent() {
       return (this.qtdTrue*100)/ this.tarefas.length | 0;
+    }
+  },
+  watch:{
+    tarefas: {
+        deep: true,
+        handler() {
+          localStorage.setItem('tarefas', JSON.stringify(this.tarefas));
+        }
     }
   },
   methods:{
@@ -63,6 +65,11 @@ export default {
     deleted(i) {
       this.tarefas.splice(i, 1)
     }
+  },
+  created() {
+      const json =  localStorage.getItem('tarefas');
+      this.tarefas =  JSON.parse(json) || []
+
   }
 }
 </script>
