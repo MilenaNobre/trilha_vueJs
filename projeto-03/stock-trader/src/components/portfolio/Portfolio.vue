4182bf44a708d5efb8ded4815da53fb7427abc82
Milena Nobre Magalhaes de Brito <milena.nobre@pampa.compasso>
2022-02-10 16:23:49 -0300

    Desenvolvimento do projeto 3

---
+++ projeto-03/stock-trader/src/components/portfolio/Portfolio.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-layout row wrap>
+        <panel v-for="stock in stocks" :key="stock.id"
+                :class-card="'blue darken-3 white--text'"
+                :class-button="'blue darken-3 white--text'"
+                :label-action="'Vender'"
+                :stock="stock"
+               :description="true"
+               @buyOrSell="sellStock"
+        ></panel>
+    </v-layout>
+</template>
+
+<script>
+import Panel from "@/components/Panel";
+
+export default {
+    name: "Portfolio",
+    components: {Panel},
+    computed:{
+        stocks(){
+            return this.$store.getters.stockPortfolio
+        }
+    },
+    methods:{
+        sellStock(obj) {
+            const order = {
+                stockId: obj.stock.id,
+                stockPrice: obj.stock.price,
+                quantity: obj.quantity
+            }
+            this.$store.dispatch('sellStock', order)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
